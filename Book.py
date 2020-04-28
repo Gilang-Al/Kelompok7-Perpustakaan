a34cdd50a552acdcbdfb95123ce68f539f59fe0b
Gilang-Al <63731624+Gilang-Al@users.noreply.github.com>
2020-04-28 22:40:07 +0800

    Update Book.py

--- Book.py
+++ Book.py
@@ -1,7 +1,11 @@
 from BookType import BookType
+from Bookorm import Book
+from librarydb import _SessionFactory
+
 class Book():
 
-    def __init__(self, BookType, Bookname, Author, Price, rackno, status = []):
+    def __init__(self, id, BookType, Bookname, Author, Price, rackno, status = []):
+        self.__id = id
         self.__BookType = BookType
         self.__Bookname = Bookname
         self.__Author = Author
@@ -55,3 +59,40 @@ class Book():
     @status.setter
     def status(self, status):
         self__status = status
+
+    @staticmethod
+    def removeBook(Bookid):
+        try:
+            session = sessionFactory()
+            session.query(Bookorm).filter_by(id=Bookid).delete()
+            session.commit()
+            session.close()
+        except Exception as R:
+            print ("->",r)
+        else:
+            print("Book Has Been Erased")
+
+        @staticmethod
+        def updateBook(idBook):
+            try:
+                newBooktype = input("Add BookType: ")
+                newBookname = input("Add New Bookname: ")
+                newAuthor = input("Add New Author : ")
+                newPrice = input("Add New Price : ")
+                newRackno = input("Add New Rackno: ")
+                newstatus = input ("Add New status: ")
+                session = sessionFactory()
+                session.query(Bookorm).filter_by(id=Bookid).update({
+                    Bookorm.Booktype: newBooktpe, Bookorm.Bookname: newBookname, Bookorm.Author: newAuthor, Bookorm.Price: newPrice, BookormRackno: NewRackno, Bookormstatus: Newstatus
+                }, synchronize_session=False)
+                session.commit()
+                session.close()
+            except Exception as r:
+                print("->",r)
+
+
+
+
+                          
+                                                                                                        
+                                  
